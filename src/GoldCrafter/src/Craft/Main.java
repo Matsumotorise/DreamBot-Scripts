ccdfb524a7952341c586cfb27990e9db0459125d
Andrew <andrewclchan211@gmail.com>
2018-05-24 11:50:38 -0400

    More walk time bank

--- src/GoldCrafter/src/Craft/Main.java
+++ src/GoldCrafter/src/Craft/Main.java
@@ -126,7 +126,7 @@ public class Main extends AbstractScript {
 
 	private void moveToBank() {  //////////////3rd state////////////////
 		getWalking().walk(bankArea.getRandomTile());
-		sleepUntil(() -> !getLocalPlayer().isMoving(), 500);
+		sleepUntil(() -> !getLocalPlayer().isMoving(), 1500);
 		sleepUntil(() -> {
 			sleep(250, 350);
 			return bankArea.contains(getLocalPlayer());
